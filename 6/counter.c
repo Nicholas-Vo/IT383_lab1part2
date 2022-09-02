635c01dbd6f7a3c65f60f3f43ac0d9bdb25b8fb7
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2022-09-01 19:29:28 -0500

    Adding part 6

---
+++ 6/counter.c
@@ -0,0 +1,17 @@
+// Nick Voss
+// IT383
+// 9/1/2022
+// 
+// Write a program that prompts the user for the name of a file.
+// Then create the file and count the number of words and lines
+// in the file and print out the counts. Use functions
+
+// Must use strtok() - Breaks string into series of tokens using a delimiter
+// Returns a pointer to the first token found in the string. Null pointer is
+// returned if no tokens are left to retrieve.
+
+// char *strtok(char *string, const char *delim)
+
+int main() {
+	return 0;
+}
